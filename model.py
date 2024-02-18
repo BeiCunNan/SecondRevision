f61608750dcc77412bddbd1f5d719a80a13201a5
Aaron <czj1240303854@163.com>
2024-02-18 13:21:54 +0800

    Initial commit

---
+++ model.py
@@ -0,0 +1,24 @@
+import math
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+
+class FNN(nn.Module):
+    def __init__(self, base_model, num_classes):
+        super().__init__()
+        self.base_model = base_model
+        self.num_classes = num_classes
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(self.base_model.config.hidden_size, num_classes)
+        )
+        for param in base_model.parameters():
+            param.requires_grad = (True)
+
+    def forward(self, inputs):
+        raw_outputs = self.base_model(**inputs)
+        cls_feats = raw_outputs.last_hidden_state[:, 0, :]
+        predicts = self.fc(cls_feats)
+        return predicts
